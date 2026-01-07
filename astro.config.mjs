ec5ec698ed35b9e150a5610b5b0928657a315ce8
PrometheanDissident <prometheandissident@gmail.com>
2026-01-06 20:27:58 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://maverickspectacle.github.io', // Your full deployed URL
+  base: '/promethean-dissident',            // Your repository name (starts with a /)
+});
